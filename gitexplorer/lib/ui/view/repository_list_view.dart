a368d61b1d65fdf6d71944f62bfb1360a03929a9
Hinrik <hinrik.helga@gmail.com>
2022-06-09 22:14:46 +0200

    Added search field - missing filter functionality

--- gitexplorer/lib/ui/view/repository_list_view.dart
+++ gitexplorer/lib/ui/view/repository_list_view.dart
@@ -1,7 +1,20 @@
 import 'package:flutter/material.dart';
 
-class RepositoryListView extends StatelessWidget {
+class RepositoryListView extends StatefulWidget {
   const RepositoryListView({ Key? key }) : super(key: key);
+
+  @override
+  State<RepositoryListView> createState() => _RepositoryListViewState();
+}
+
+class _RepositoryListViewState extends State<RepositoryListView> {
+  late String search;
+
+  @override
+  void initState() {
+    super.initState();
+    search = '';
+  }
 
   Widget _buildHeading() {
     return const Text(
@@ -14,15 +27,85 @@ class RepositoryListView extends StatelessWidget {
     );
   }
 
+  
+
   @override
   Widget build(BuildContext context) {
     return Padding(
-      padding: const EdgeInsets.symmetric(horizontal: 12.0),
+      padding: const EdgeInsets.symmetric(horizontal: 20.0),
       child: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
         children: [
           const SizedBox(height: 100.0,),
-          _buildHeading()
+          _buildHeading(),
+          _SearchField(
+            onChange: (value) {
+              setState(() {
+                search = value;
+              });
+            },
+          ),
+          Center(
+            child: Text(
+              search
+            ),
+          )
         ],
+      ),
+    );
+  }
+}
+
+class _SearchField extends StatefulWidget {
+  final Function(String) onChange;
+
+  const _SearchField({ Key? key, required this.onChange }) : super(key: key);
+
+  @override
+  State<_SearchField> createState() => _SearchFieldState();
+}
+
+class _SearchFieldState extends State<_SearchField> {
+  late TextEditingController _searchRepoController;
+
+  @override
+  void initState() {
+    super.initState();
+    _searchRepoController = TextEditingController();
+  }
+
+  @override
+  void dispose() {
+    _searchRepoController.dispose();  
+    super.dispose();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: const EdgeInsets.symmetric(vertical: 20.0),
+      child: TextField(
+        controller: _searchRepoController,
+        decoration: InputDecoration(
+          contentPadding: const EdgeInsets.all(8),
+          enabledBorder: OutlineInputBorder(
+            borderRadius: BorderRadius.circular(8.0),
+            borderSide: const BorderSide(style: BorderStyle.none),
+          ),
+          focusedBorder: OutlineInputBorder(
+            borderRadius: BorderRadius.circular(8.0),
+            borderSide: const BorderSide(style: BorderStyle.none),
+          ),
+          filled: true,
+          fillColor: const Color.fromRGBO(242, 243, 247, 1),
+          prefixIcon: const Icon(
+            Icons.search,
+            color: Colors.black38,
+          ),
+          hintText: 'Search for repository'
+        ),
+        cursorColor: Colors.black54,
+        onChanged: widget.onChange,
       ),
     );
   }
